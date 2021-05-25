2612e88b6420caf9ade9e9e49ae239ba1bcdcbbb
EvgeniiMorozov <emorozov1101@gmail.com>
2021-05-25 20:01:56 +0500

    Редактирование класса HorizontalBar

--- shooter/game_object/horizontal_bar.py
+++ shooter/game_object/horizontal_bar.py
@@ -2,9 +2,17 @@ import pygame
 
 
 class HorizontalBar:
-    def __init__(self, x, y):
-        self.rect.x = x
-        self.rect.y = y
+    def __init__(self, x, y, parameter, length, height, fill_color, outline_color):
+        self.x = x
+        self.y = y
+        self.parameter = parameter
+        self.length = length
+        self.height = height
+        self.fill = (self.parameter / 100) * self.length
+        self.outline_color = outline_color
+        self.fill_color = fill_color
+        self.outline_rect = pygame.Rect(self.x, self.y, self.length, self.height)
+        self.fill_rect = pygame.Rect(self.x, self.y, self.fill, self.height)
 
     def blit(self, surface):
         surface.blit()
