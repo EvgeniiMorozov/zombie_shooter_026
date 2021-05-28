c6313fc723929f72b1e8658aa729393bb70f6167
Evgenii Morozov <zheka1101@yandex.ru>
2021-05-28 09:36:51 +0500

    Добавлено описание переменных

--- shooter/game_object/horizontal_bar.py
+++ shooter/game_object/horizontal_bar.py
@@ -3,6 +3,15 @@ import pygame
 
 class HorizontalBar:
     def __init__(self, x, y, parameter, length, height, fill_color, outline_color):
+        """
+        x - координата х
+        н - координата y
+        parameter - параметр, который будет визуализирован
+        length - длина прямоугольника
+        height - высота прямоугольника
+        fill_color - цвет внутреннего прямоугольника
+        outline_color - цвет внешного (нижнего) прямоугольника
+        """
         self.x = x
         self.y = y
         self.parameter = parameter
@@ -13,6 +22,9 @@ class HorizontalBar:
         self.fill_color = fill_color
         self.outline_rect = pygame.Rect(self.x, self.y, self.length, self.height)
         self.fill_rect = pygame.Rect(self.x, self.y, self.fill, self.height)
+
+    def parameter_scaling(self):
+        pass
 
     def blit(self, surface):
         pygame.draw.rect(surface, self.outline_color, self.outline_rect, 2)
